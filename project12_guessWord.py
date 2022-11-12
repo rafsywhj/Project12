e0eda1e4a33a17b26dc62f33b0b6042196f1ab47
rafsywhj <70297474+rafsywhj@users.noreply.github.com>
2022-11-12 14:27:12 +0400

    Add files via upload

---
+++ project12_guessWord.py
@@ -0,0 +1,13 @@
+#---------------GUESS THE FIVE LETTER WORD----------------------------#
+
+#create a variable for life as 9
+# create a list of 5 leter words
+# Let the system chooses a random word from the list and save inside a variable guessWord
+
+# Show the output with 5 question mark. Eg:- ['?', '?', '?', '?', '?']
+# Show the lives left. Eg:- Lives left: ❤❤❤❤❤❤❤❤❤
+
+#Ask the user to enter the letter
+# If the letter is there in the guessWord, replace '?' with the letter.
+# else reduce the life
+# This should continue until the life gets to zero or the write word predicted.
